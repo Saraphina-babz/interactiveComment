6d74a76aa30eedf7140fe1777a646f26f04602b7
Saraphina-babz <oyeyemibunmi21@gmail.com>
2024-07-10 16:26:42 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import CommentSection from './Components/CommentSection';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CommentSection />
+    </div>
+  );
+}
+
+export default App;
